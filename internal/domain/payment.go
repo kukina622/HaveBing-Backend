c1b08d647b48baebec27746fdddb3474a42be156
Min <wyes91622@gmail.com>
2023-06-15 03:16:19 +0800

    feat: payment status to enum

--- internal/domain/payment.go
+++ internal/domain/payment.go
@@ -7,12 +7,19 @@ import (
 	"github.com/shopspring/decimal"
 )
 
+type paymentStatus string
+
+const (
+	PaymentStatusUnpaid paymentStatus = "unpaid"
+	PaymentStatusPaid   paymentStatus = "paid"
+)
+
 type Payment struct {
 	ID            uint            `gorm:"type:bigint(20) NOT NULL auto_increment;primary_key;" json:"paymentId"`
 	PaymentDate   *time.Time      `gorm:"type:datetime"`
 	ShippingFee   decimal.Decimal `gorm:"type:decimal(19,4) NOT NULL;"`
 	Amount        decimal.Decimal `gorm:"type:decimal(19,4) NOT NULL;"`
-	PaymentStatus string          `gorm:"type:varchar(255) NOT NULL;"`
+	PaymentStatus paymentStatus   `gorm:"type:ENUM('unpaid', 'paid') NOT NULL;"`
 	OrderId       uint            `gorm:"type:bigint(20) NOT NULL;index:idx_order_id;" json:"orderId"`
 }
 
