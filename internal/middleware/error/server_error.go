37d720e2f5e562166722ea97f19837bf75dca481
Min <wyes91622@gmail.com>
2023-06-24 00:33:23 +0800

    feat: add server common error message

--- internal/middleware/error/server_error.go
+++ internal/middleware/error/server_error.go
@@ -1,5 +1,13 @@
 package error
 
+const (
+	MISSING_PARAMETER      = "The request is missing a required parameter. Please provide the correct parameter and try again"
+	UNAUTHORIZED           = "Unauthorized"
+	RESOURCE_NOT_FOUND     = "The requested resource was not found."
+	CREATE_RESOURCE_FAILED = "Failed to create the data"
+	UPDATE_RESOURCE_FAILED = "Failed to update the data"
+)
+
 type ServerError struct {
 	Code int    `json:"code"`
 	Msg  string `json:"msg"`
